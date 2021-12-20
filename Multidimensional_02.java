876400a84cd6f256354071549a0317f49ade3973
“K-takashii” <nanase.sigre.1627.yorushika@gmail.com>
2021-12-20 14:56:35 +0900

    Initial commit

--- Multidimensional_02.java
+++ Multidimensional_02.java
@@ -1,6 +1,8 @@
 class Multidimensional_02 {
     public static void main(final String[] args){
         String[][] arr = {{"sato","suzuki"},{"takahashi","tanaka"}};
+        //1次元配列のように省略して書くことができる。
+        //２次元配列の場合は、波括弧の中の波括弧でデータを代入できる。
 
         System.out.println(arr[0][0]);
         System.out.println(arr[1][0]);
